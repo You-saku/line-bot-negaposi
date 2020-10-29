692c04d540f4819ac815ba9ce033bcff86fc18ca
You-saku <usakusekine@gmail.com>
2020-10-29 22:14:46 +0900

    third commit

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # LINE_bot_sample
+<h1>2020/10/19</h1>
+リポジトリスタート
+
+<h1>2020/10/29</h1>
+リポジトリを改善<br>
+<strong>ネガポジ分析を取り入れてみた</strong><br>
+ネガティブワード、ポジティブワードの辞書は(dictonary/pn.csvのcsvファイルの著作権も同様)<br>
+<br>
+"小林のぞみ，乾健太郎，松本裕治，立石健二，福島俊一. 意見抽出のための評価表現の収集. 自然言語処理，Vol.12, No.3, pp.203-222, 2005. / Nozomi Kobayashi, Kentaro Inui, Yuji Matsumoto, Kenji Tateishi. Collecting Evaluative Expressions for Opinion Extraction, Journal of Natural Language Processing 12(3), 203-222, 2005."<br>
+で紹介している<br>
+<br>
+"日本語評価極性辞書（名詞編）ver.1.0（2008年12月版）/ Japanese Sentiment Dictionary (Volume of Nouns) ver. 1.0  著作者: 東北大学 乾・岡崎研究室 / Author(s): Inui-Okazaki Laboratory, Tohoku University"<br>
+
+を使わせていただきました。ありがとうございます。<br>
--- app.py
+++ app.py
@@ -11,9 +11,8 @@ from linebot.models import (
 )
 import os
 
-import message as mess
-
-
+from packages import message as mess
+from packages import negaposi as np
 app = Flask(__name__)
 
 #os.environ -> 環境変数からもってくる　アクセストークンの記述の必要なし
@@ -41,6 +40,7 @@ def callback():
 
     # handle webhook body
     try:
+        #webhookが正常ならば  handleに登録されている関数の実施を行う
         handler.handle(body, signature)
     except InvalidSignatureError:
         print("Invalid signature. Please check your channel access token/channel secret.")
@@ -48,15 +48,27 @@ def callback():
 
     return 'OK'
 
-
+#イベントが発生した場合
+#handleに付与する関数の記述
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    responce = mess.kusoripu()
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=responce))
+    #responce = mess.kusoripu()
+    text = event.message.text
+    if len(text) > 30:
+        responce = np.negaposi_check(text)
+        #pythonの改行コードは [\n]
+        cresit = "\n\n『小林のぞみ，乾健太郎，松本裕治，立石健二，福島俊一. 意見抽出のための評価表現の収集. 自然言語処理，Vol.12, No.3, pp.203-222, 2005. / Nozomi Kobayashi, Kentaro Inui, Yuji Matsumoto, Kenji Tateishi. Collecting Evaluative Expressions for Opinion Extraction, Journal of Natural Language Processing 12(3), 203-222, 2005.』参照"
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text="ネガポジ判別結果\n"+responce+cresit))
+    else:
+        responce = "大丈夫だよ。\nそんな短い文章送ってくるならまだ余裕がある証拠だ。"
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=responce))
 
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT",5000))#os.getenv -> 環境変数の数字を取得　引数2つめのおかげで仮に環境変数がヒットしなくてもその引数を設定してくれる
+    #0.0.0.0 = どこからでも通信可能なやつ
     app.run(host="0.0.0.0",port=port)
